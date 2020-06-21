1cd11f7a77a60eefdf966eb9bc933c5c5d805430
willisrc <63082788+willisrc@users.noreply.github.com>
2020-06-21 00:35:33 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ const client = new Discord.Client();
 client.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    console.log(message);
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
+    console.log(message.content);
+    if (message.content.substring(0, 1) == '!') {
+        var args = message.content.substring(1).split(' ');
         var cmd = args[0];
        
         args = args.splice(1);
