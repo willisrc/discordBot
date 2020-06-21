5f0373b3458c934f1a2b6b83da7af4e277897186
willisrc <63082788+willisrc@users.noreply.github.com>
2020-06-21 00:13:19 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,9 +14,9 @@ client.on('ready', () => {
 
 client.on('message', message => {
 
-    if (message.content === 'ping') {
+    if (message.content === 'daddy') {
 
-       message.reply('pong');
+       message.reply('yes?');
 
        }
 
