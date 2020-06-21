be0db4fc5a4cebbf7bf08c5402ec4de6f5532940
willisrc <63082788+willisrc@users.noreply.github.com>
2020-06-21 01:00:59 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,8 +4,8 @@ const client = new Discord.Client();
 client.on('message', message => {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    console.log('message' + message.channel.id);
-    const channel = client.channels.cache.get(message.channel.id);
+    //console.log('message' + message.channel.id);
+    //const channel = client.channels.cache.get(message.channel.id);
     //console.log(message);
     if (message.content.substring(0, 1) == '!') {
         var args = message.content.substring(1).split(' ');
@@ -15,7 +15,7 @@ client.on('message', message => {
         switch(cmd) {
             // !help
 	    case 'help':
-                channel.send('UwU here is what I can do for you, senpai: \n\t!link ---> gimme those wacky 6 digits and I will give you the sauce \n\t!random ---> Your risky click of the day');
+                message.channel.send('UwU here is what I can do for you, senpai: \n\t!link ---> gimme those wacky 6 digits and I will give you the sauce \n\t!random ---> Your risky click of the day');
             break;
 			
 	    // !sauce
