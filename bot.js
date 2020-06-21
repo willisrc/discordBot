0881c6f041606937d0a55bd178cf154fc78af8d3
willisrc <63082788+willisrc@users.noreply.github.com>
2020-06-21 01:03:05 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,7 @@ const client = new Discord.Client();
 client.on('message', message => {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    //console.log('message' + message.channel.id);
-    //const channel = client.channels.cache.get(message.channel.id);
-    //console.log(message);
+    console.log(message);
     if (message.content.substring(0, 1) == '!') {
         var args = message.content.substring(1).split(' ');
         var cmd = args[0];
@@ -16,23 +14,15 @@ client.on('message', message => {
             // !help
 	    case 'help':
                 message.channel.send('UwU here is what I can do for you, senpai: \n\t!link ---> gimme those wacky 6 digits and I will give you the sauce \n\t!random ---> Your risky click of the day');
-            break;
-			
+            break;			
 	    // !sauce
             case 'link':
-                client.sendMessage({
-                    to: channelID,
-                    message: 'http://www.nhentai.com/g/' + args
-                });
-            break;
-			
+                    message.channel.send('http://www.nhentai.com/g/' + args);
+            break;		
 	    // !random
 	    case 'random':
                 var i = Math.floor(Math.random() * 1000000);
-		client.sendMessage({
-		    to: channelID,
-                    message: 'http://www.nhentai.com/g/' + i
-                });
+		message.channel.send('http://www.nhentai.com/g/' + i);
             break;
             // Just add any case commands if you want to..
          }
