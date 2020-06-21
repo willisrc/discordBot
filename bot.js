cbcd58042a067ad15c9210bca1459a36cd86238f
willisrc <63082788+willisrc@users.noreply.github.com>
2020-06-21 00:31:05 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('Ready to give the sauce');
-});
-
-client.on('message', message => {
-    //console.log(message);
-    if (message.content === 'daddy') {
-       message.reply('yes?');
-       }
- 
+client.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
@@ -18,23 +11,25 @@ client.on('message', message => {
         args = args.splice(1);
         switch(cmd) {
             // !help
-			         case 'help':
+	    case 'help':
                 client.sendMessage({
                     to: channelID,
                     message: 'UwU here is what I can do for you, senpai: \n\t!link ---> gimme those wacky 6 digits and I will give you the sauce \n\t!random ---> Your risky click of the day'
                 });
             break;
-			         // !sauce
+			
+	    // !sauce
             case 'link':
                 client.sendMessage({
                     to: channelID,
                     message: 'http://www.nhentai.com/g/' + args
                 });
             break;
-            // !random
-            case 'random':
+			
+	    // !random
+	    case 'random':
                 var i = Math.floor(Math.random() * 1000000);
-				            bot.sendMessage({
+				bot.sendMessage({
                     to: channelID,
                     message: 'http://www.nhentai.com/g/' + i
                 });
