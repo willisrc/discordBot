67e8a1d943debeb926e23a7b639ae2143fb9e5fa
Will Cronin <croninw9@students.rowan.edu>
2020-06-22 13:40:21 -0400

    testing response messages

--- bot.js
+++ bot.js
@@ -2,32 +2,33 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('message', message => {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
     console.log(message);
+    // Command Messages - Starting with `!`
     if (message.content.substring(0, 1) == '!') {
         var args = message.content.substring(1).split(' ');
         var cmd = args[0];
-       
         args = args.splice(1);
+
         switch(cmd) {
-            // !help
-	    case 'help':
-                message.channel.send('UwU here is what I can do for you, senpai: \n\t!link ---> gimme those wacky 6 digits and I will give you the sauce \n\t!random ---> Your risky click of the day');
-            break;			
-	    // !sauce
-            case 'link':
-                    message.channel.send('http://www.nhentai.com/g/' + args);
-            break;		
-	    // !random
-	    case 'random':
-                var i = Math.floor(Math.random() * 1000000);
-		message.channel.send('http://www.nhentai.com/g/' + i);
-            break;
-            // Just add any case commands if you want to..
+          // !help
+    	    case 'help':
+            message.channel.send('UwU here is what I can do for you, senpai: \n\t!link ---> gimme those wacky 6 digits and I will give you the sauce \n\t!random ---> Your risky click of the day');
+          break;
+    	    // !link
+          case 'link':
+            message.channel.send('http://www.nhentai.com/g/' + args);
+          break;
+    	    // !random
+    	    case 'random':
+            var i = Math.floor(Math.random() * 1000000);
+            message.channel.send('http://www.nhentai.com/g/' + i);
+          break;
          }
+     }
+     // Response Messages
+     if (message.content.contains('anime')) {
+       message.channel.send('Anime is a sin');
      }
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);
