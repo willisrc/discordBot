c7e6f4dd88a4c190c0d1f012c682da81d66f22f9
Will Cronin <croninw9@students.rowan.edu>
2020-06-22 16:33:06 -0400

    copy pasta 3.0

--- bot.js
+++ bot.js
@@ -30,15 +30,7 @@ client.on('message', message => {
           break;
           // !copypasta
           case 'copypasta':
-            const fs = require('fs')
-            fs.readFile('files/copypastas.txt', (err, data) => {
-              if (err) throw err;
-              console.log(data.toString());
-
-              var arr = data.toString().split('\n');
-              var i = Math.floor(Math.random() * arr.length);
-              message.channel.send(arr[i]);
-            })    
+            message.channel.send(copypasta());
           break;
          }
      }
@@ -63,3 +55,14 @@ client.on('message', message => {
 client.login(process.env.BOT_TOKEN);
 
 // FUNCTIONS
+function copypasta() {
+  const fs = require('fs')
+  fs.readFile('files/copypastas.txt', (err, data) => {
+    if (err) throw err;
+    //console.log(data.toString());
+
+    var arr = data.toString().split('\n');
+    var i = Math.floor(Math.random() * arr.length);
+    return arr[i]);
+  })
+}
