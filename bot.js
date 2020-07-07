a07d9713e66c42ddc74cce370c7aa6b29d9962d7
Will Cronin <croninw9@students.rowan.edu>
2020-07-06 21:16:02 -0400

    embed test

--- bot.js
+++ bot.js
@@ -97,10 +97,10 @@ client.on('message', message => {
             .setColor('#0099ff')
             .setTitle('Sup pussies')
             .setURL('https://discord.js.org/')
-            .setAuthor('Slack-chan', 'images/ahegao_stonks.jpg', 'https://discord.js.org')
+            .setAuthor('Slack-chan', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
             .setDescription('Konichiwa Senpai! Slack-chan is here to fullfill all of your needs! This bot was made with the purpose of having a Slackbot equivalent in Discord! All of your favorite commands have been moved to here! Here is what I can currently do: ' +
               '\n (Slack-chan is currently in beta)')
-            .setThumbnail('images/ahegao_stonks.jpg')
+            .setThumbnail('https://i.imgur.com/wSTFkRM.png')
             .addFields(
               { name: 'Regular field title', value: 'Some value here' },
               { name: '\u200B', value: '\u200B' },
