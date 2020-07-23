b3317d15e0120d756e5e0cb34998abe63eee1a64
Will Cronin <croninw9@students.rowan.edu>
2020-07-23 17:35:36 -0400

    roll command update

--- bot.js
+++ bot.js
@@ -5,9 +5,9 @@ const Waifu = require('./waifuScript/waifu');
 
 const responseObject = {
   "stocks": {files: ['./images/ahegao_stonks.jpg']},
-  'slack-chan': "Say what?",
+  'slack-chan': "You rang?",
   "fitness": "You can go ahead and fittin\' this dick in your mouth",
-  "vibe check": "You have failed the Vibe check",
+  "vibe": "You have failed the Vibe check",
   "bleach": "an engineer\'s favorite drink!",
   "anime": "Anime is a sin",
   "rawr": "Rawr x3 nuzzles how are you pounces on you you\'re so warm o3o notices you have a bulge o: someone\'s happy ;) nuzzles your necky wecky~ murr~ hehehe rubbies your bulgy wolgy you\'re so big :oooo rubbies more on your bulgy wolgy it doesn\'t stop growing ·///· kisses you and lickies your necky daddy likies (; nuzzles wuzzles I hope daddy really likes $: wiggles butt and squirms I want to see your big daddy meat~ wiggles butt I have a little itch o3o wags tail can you please get my itch~ puts paws on your chest nyea~ its a seven inch itch rubs your chest can you help me pwease squirms pwetty pwease sad face I need to be punished runs paws down your chest and bites lip like I need to be punished really good~ paws on your bulge as I lick my lips I\'m getting thirsty. I can go for some milk unbuttons your pants as my eyes glow you smell so musky :v licks shaft mmmm~ so musky drools all over your cock your daddy meat I like fondles Mr. Fuzzy Balls hehe puts snout on balls and inhales deeply oh god im so hard~ licks balls punish me daddy~ nyea~ squirms more and wiggles butt I love your musky goodness bites lip please punish me licks lips nyea~ suckles on your tip so good licks pre of your cock salty goodness~ eyes role back and goes balls deep mmmm~ moans and suckles"
@@ -16,8 +16,12 @@ const responseObject = {
 
 client.on('message', message => {
     //Response Messages
-    if(responseObject[message.content]) {
-      message.channel.send(responseObject[message.content]);
+    var arr = message.content.split(' ');
+    var x;
+    for (x in arr) {
+      if(responseObject[message.content]) {
+        message.channel.send(responseObject[message.content]);
+      }
     }
 
     // Command Messages - Starting with `!`
