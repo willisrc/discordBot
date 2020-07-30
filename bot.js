f6f1023058708ca3bb953cc23246ddd21705c6ba
Will Cronin <croninw9@students.rowan.edu>
2020-07-30 15:37:22 -0400

    more responses

--- bot.js
+++ bot.js
@@ -12,12 +12,14 @@ const responseObject = {
   "anime": "Anime is a sin",
   "rawr": "Rawr x3 nuzzles how are you pounces on you you\'re so warm o3o notices you have a bulge o: someone\'s happy ;) nuzzles your necky wecky~ murr~ hehehe rubbies your bulgy wolgy you\'re so big :oooo rubbies more on your bulgy wolgy it doesn\'t stop growing ·///· kisses you and lickies your necky daddy likies (; nuzzles wuzzles I hope daddy really likes $: wiggles butt and squirms I want to see your big daddy meat~ wiggles butt I have a little itch o3o wags tail can you please get my itch~ puts paws on your chest nyea~ its a seven inch itch rubs your chest can you help me pwease squirms pwetty pwease sad face I need to be punished runs paws down your chest and bites lip like I need to be punished really good~ paws on your bulge as I lick my lips I\'m getting thirsty. I can go for some milk unbuttons your pants as my eyes glow you smell so musky :v licks shaft mmmm~ so musky drools all over your cock your daddy meat I like fondles Mr. Fuzzy Balls hehe puts snout on balls and inhales deeply oh god im so hard~ licks balls punish me daddy~ nyea~ squirms more and wiggles butt I love your musky goodness bites lip please punish me licks lips nyea~ suckles on your tip so good licks pre of your cock salty goodness~ eyes role back and goes balls deep mmmm~ moans and suckles",
   "meme": "bottom text",
+  "bot": "I may be a bot, but I'm less of a virgin than you",
   "kira": "Watashi no na wa Kira Yoshikage. Nenre san-juu-san sai. Jitaku wa Morioh-cho hokuto bu no bessou chitai ni ari, kekkon wa shiteinai. Shigoto wa Kame Yu chuun den no kaishain de mainichi osuku tomo yoru hachi ji made niwa kitakusuru. Tabako wa suwanai, sake wa tashinamu tedo. Yoru juu-ichi niwa toko ni tsuki, kanarazu hachi jikan wa suimin o toru you ni shiteiru. Neru mae ni atatakai miruku o nomi. Nijuu bun hodo no sutorecchi de karada o hogushite kara toko ni tsukuto. Hotondo asa made jukusui sai. Akanbou no you ni hirou ya sutoresu o nokosazu ni, asa me o samaserun da. Kenkou shindan demo ijou na shito iwaretai you. Watashi wa tsuneni kokoro no heon o negatte ikiteru ningen toiu koto o setsume shiteiru noda yo. \n \nKachimake ni kodawattari atama o kakaeru you na toraburu toka yoru mo nemurenai toitta teki o tsukuranai... toiu no ga, watashi no shakai ni taisuru shise de ari, sore ga jibun no koufuku da toiu koto o shitteiru. Mottomo tatakatta toshitemo watashi wa dare nimo maken gane.",
   "classified": {files: ['./images/its_classified.gif']},
   "Lockheed": {files: ['./images/its_classified.gif']},
   "lockheed": {files: ['./images/its_classified.gif']},
   "Grumann": {files: ['./images/its_classified.gif']},
-  "Lockheed Martin": {files: ['./images/its_classified.gif']}
+  "Lockheed Martin": {files: ['./images/its_classified.gif']},
+  "Chadkola": {files: ['./images/chadkola.png']}
 };
 
 
